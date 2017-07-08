65aa573da616754493a83e16561ed13c370d7755
omarselim3008 <omar@intellectionsoftware.com>
2017-07-08 18:36:12 +0100

    ooops

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,6 @@
 class CustomersController < ApplicationController
   def index
   	search = params['keywords']
-  	byebug
   	@customers = if search.present?
   	  Customer.where("lower(first_name) LIKE :first_name", {first_name: search})
   	else
