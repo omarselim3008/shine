843af9381a1257d0bd548e781b0434563944ffff
omarselim3008 <omar@intellectionsoftware.com>
2017-07-08 16:32:43 +0100

    Add shit search

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,11 @@
 class CustomersController < ApplicationController
   def index
-  	@customers = Customer.all.limit(10)
+  	search = params['keywords']
+  	byebug
+  	@customers = if search.present?
+  	  Customer.where("lower(first_name) LIKE :first_name", {first_name: search})
+  	else
+  	  Customer.all.limit(10)
+  	end
   end
 end
