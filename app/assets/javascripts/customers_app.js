422c2d8163c317baa628346e40057569c301d7a1
omarselim3008 <omar@intellectionsoftware.com>
2017-07-15 12:16:55 +0100

    Canned results

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -3,8 +3,25 @@ var app = angular.module('customers',[]);
 app.controller("CustomerSearchController", [
 	"$scope",
   function($scope) {
+  	$scope.customers = [];
 	  $scope.search = function(searchTerm){
-		  $scope.searchFor = searchTerm;
+		  //$scope.searchFor = searchTerm;
+		  $scope.customers = [
+		  	{
+		  		"first_name": "Bob",
+		  		"last_name": "James",
+		  		"email": "email@email.com",
+		  		"username": "bo23",
+		  		"created_at": "2017-07-24"
+		  	},
+		  	{
+		  		"first_name": "Bob2",
+		  		"last_name": "James2",
+		  		"email": "email2@email.com",
+		  		"username": "bo232",
+		  		"created_at": "2012-07-24"
+		  	},
+		  ]
 	  }
   }
 ]);
