71cb46b8e95e6f271e9ff21d0540bb3057496839
omarselim3008 <omar@intellectionsoftware.com>
2017-07-16 12:10:51 +0100

    Add spec for search

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -30,7 +30,7 @@ class CustomerSearchTerm
   end
 
   def extract_name(email)
-  	email.gsub(/@. *$/, '').gsub(/[0-9]+/,'')
+  	email.gsub(/@.*$/, '').gsub(/[0-9]+/,'')
   end
 
   def build_for_email_search(search_term)
