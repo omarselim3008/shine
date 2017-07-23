0b30b708aa5d8a2919892066e44dcbb907783264
omarselim3008 <omar@intellectionsoftware.com>
2017-07-23 10:27:10 +0100

    Get specs goinf for real this time

--- spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
+++ spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
@@ -16,18 +16,42 @@ describe("CustomSearchController", function() {
 	  	expect(scope.customers).toEqualData([]);
 	  });
 	});
-});
 
-beforeEach(function(){
-	jasmine.addMatchers({
-		toEqualData: functon(util, customEqualityTesters) {
-			return {
-	      compare: function(actual, expected) {
-		      var result = {};
-		      result.pass = angular.equals(actual, expected);
-		      return result;
-	 	    }
-		  };
-	  }
-  });
+
+	describe("Fetching search results", function() {
+
+	  var scope       = null,
+	      httpBackend = null,
+	      controller  = null,
+	      serverResults = [
+	      		{
+	      			id: 123,
+	      			first_name: "Bob",
+	      			last_name: "Jones",
+	      			email: "bjones@foo.net",
+	      			username: "jonesy"
+	      		},
+	      		{
+	      			id: 234,
+	      			first_name: "Bob",
+	      			last_name: "Johnsons",
+	      			email: "johnboy@bar.net",
+	      			username: "bobj"
+	      		}
+	        ];
+
+	  beforeEach(module("customers"));
+
+	  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+	  			scope = $rootScope.$new();
+	  			httpBackend = $httpBackend;
+	  			controller = $controller("CustomerSearchController", {
+	  			  $scope: scope
+	  			});
+	      }));
+	  it("populates the customer list with results", function() {
+	  	debugger;
+	  	expect(scope.customers).toEqualData([]);
+	  });
+	});
 });
