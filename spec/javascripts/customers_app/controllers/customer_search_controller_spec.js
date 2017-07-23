d0ccbea325d0ac9cdd07900448e6bdcbf00c1233
omarselim3008 <omar@intellectionsoftware.com>
2017-07-23 13:21:59 +0100

    Add Error test

--- spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
+++ spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
@@ -18,6 +18,51 @@ describe("CustomSearchController", function() {
 	});
 
 
+	describe("Errors", function() {
+
+	  var scope       = null,
+	      controller  = null,
+	      httpBackend = null,
+	      serverResults = [
+	      		{
+	      			id: 123,
+	      			first_name: "Bob",
+	      			last_name: "Jones",
+	      			email: "bjones@foo.net",
+	      			username: "jonesy"
+	      		},
+	      		{
+	      			id: 234,
+	      			first_name: "Bob",
+	      			last_name: "Johnsons",
+	      			email: "johnboy@bar.net",
+	      			username: "bobj"
+	      		}
+	        ];
+
+	  beforeEach(module("customers"));
+
+	  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+	  			scope = $rootScope.$new();
+	  			httpBackend = $httpBackend;
+	  			controller = $controller("CustomerSearchController", {
+	  			  $scope: scope
+	  			});
+	      }));
+
+		beforeEach(function() {
+			httpBackend.when("GET", "/customers.json?keywords=bob&page=0").respond(500, "Internal Server Error");
+			spyOn(window, "alert");
+		});
+
+	  it("populates the customer list with results", function() {
+	  	scope.search("bob");
+	  	httpBackend.flush();
+	  	expect(scope.customers).toEqualData([]);
+	  	expect(window.alert).toHaveBeenCalledWith("There was a problem: 500")
+	  });
+	});
+
 	describe("Fetching search results", function() {
 
 	  var scope       = null,
